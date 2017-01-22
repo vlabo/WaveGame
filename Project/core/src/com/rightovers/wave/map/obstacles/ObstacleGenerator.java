ac249e9417719044ee6e231916f4ad03d5c53334
Vladimir <vladdocan@gmail.com>
2017-01-22 15:26:56 +0200

    last stuff

--- Project/core/src/com/rightovers/wave/map/obstacles/ObstacleGenerator.java
+++ Project/core/src/com/rightovers/wave/map/obstacles/ObstacleGenerator.java
@@ -37,7 +37,7 @@ public class ObstacleGenerator {
     public void update(float delta) {
         this.time += delta;
 
-        if (this.time >= (this.random.nextFloat() * 15 + 1.5)) {
+        if (this.time >= (this.random.nextFloat() * 20 + 3)) {
 
             this.obstacles.add(createObstacle(random.nextInt(buldingsSizes.size() - 1)));
             this.time = 0;
