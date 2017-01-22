79098d2f7b18f76e993c84d7134004cd23003dfe
Yasen Bagalev <jabaga@abv.bg>
2017-01-22 11:02:50 +0200

    bg

--- Project/core/src/com/rightovers/wave/map/Environment.java
+++ Project/core/src/com/rightovers/wave/map/Environment.java
@@ -1,7 +1,7 @@
 package com.rightovers.wave.map;
 
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.TextureRegion;
 import com.badlogic.gdx.math.Rectangle;
 import com.rightovers.wave.Main;
 import com.rightovers.wave.map.obstacles.ObstacleGenerator;
@@ -17,12 +17,11 @@ import java.util.List;
 public class Environment implements IResourceable {
 
     private static final String BACKGROUND_IMAGE = "images/background.png";
-    private static final String GROUND_IMAGE = "images/ground.png";
     public static final String BUILDINGS = "images/buildings.pack";
 
 
-    private Texture background = null;
-    private Texture ground = null;
+    private TextureRegion background = null;
+    private TextureRegion ground = null;
     public final float GROUND_LEVEL = Funcs.percentHeight(5);
     public final float VISIBLE_X_METERS = 150;
 
@@ -47,10 +46,32 @@ public class Environment implements IResourceable {
 
     public void loadAssets() {
         Loader.getInstance().addAsset(Main.getInstance().assetsGroupName, BACKGROUND_IMAGE, Loader.AssetType.TEXTURE);
-        Loader.getInstance().addAsset(Main.getInstance().assetsGroupName, GROUND_IMAGE, Loader.AssetType.TEXTURE);
         Loader.getInstance().addAsset(Main.getInstance().assetsGroupName, BUILDINGS, Loader.AssetType.TEXTURE_ATLAS);
 
         this.generator = ObstacleGenerator.getInstance();
+    }
+
+    public void init() {
+        if (this.background == null) {
+            this.background = new TextureRegion(Loader.getInstance().getTexture(BACKGROUND_IMAGE));
+            this.backgroundPositions = new ArrayList<Rectangle>();
+            this.backgroundRatio = (float) this.background.getRegionHeight() / (float) Main.getInstance().height;
+
+            int backgroundFitTimes = (int) (Main.getInstance().width * this.backgroundRatio / this.background.getRegionHeight()) + 2;
+            Gdx.app.log("ENV", this.backgroundRatio + "  " + backgroundFitTimes);
+            for (int i = 0; i < backgroundFitTimes; i++) {
+                this.backgroundPositions.add(new Rectangle(i * (this.background.getRegionWidth() / this.backgroundRatio), 0, this.background.getRegionWidth() / this.backgroundRatio, this.background.getRegionHeight() / this.backgroundRatio));
+            }
+        }
+
+        if (this.ground == null) {
+            this.ground = Loader.getInstance().getTextureAtlas(BUILDINGS).findRegion("7");
+            this.groundPositions = new ArrayList<Rectangle>();
+            int groundFitTimes = (int) (Main.getInstance().width / this.ground.getRegionWidth()) + 2;
+            for (int i = 0; i < groundFitTimes; i++) {
+                this.groundPositions.add(new Rectangle(i * this.ground.getRegionWidth(), 0, this.ground.getRegionWidth(), this.ground.getRegionHeight()));
+            }
+        }
     }
 
 
@@ -63,7 +84,7 @@ public class Environment implements IResourceable {
 
     }
 
-    private void drawTextureSequence(Texture texture, List<Rectangle> positions, float speed) {
+    private void drawTextureSequence(TextureRegion texture, List<Rectangle> positions, float speed) {
         boolean shouldMoveFirst = false;
 
         for (Rectangle currentPosition : positions) {
@@ -93,25 +114,9 @@ public class Environment implements IResourceable {
     }
 
     public void update(float delta) {
+        // TODO move this
         if (this.background == null) {
-            this.background = Loader.getInstance().getTexture(BACKGROUND_IMAGE);
-            this.backgroundPositions = new ArrayList<Rectangle>();
-            this.backgroundRatio = (float) this.background.getHeight() / (float) Main.getInstance().height;
-
-            int backgroundFitTimes = (int) (Main.getInstance().width * this.backgroundRatio / this.background.getHeight()) + 2;
-            Gdx.app.log("ENV", this.backgroundRatio + "  " + backgroundFitTimes);
-            for (int i = 0; i < backgroundFitTimes; i++) {
-                this.backgroundPositions.add(new Rectangle(i * (this.background.getWidth() / this.backgroundRatio), 0, this.background.getWidth() / this.backgroundRatio, this.background.getHeight() / this.backgroundRatio));
-            }
-        }
-
-        if (this.ground == null) {
-            this.ground = Loader.getInstance().getTexture(GROUND_IMAGE);
-            this.groundPositions = new ArrayList<Rectangle>();
-            int groundFitTimes = (int) (Main.getInstance().width / this.ground.getWidth()) + 2;
-            for (int i = 0; i < groundFitTimes; i++) {
-                this.groundPositions.add(new Rectangle(i * this.ground.getWidth(), 0, this.ground.getWidth(), this.ground.getHeight()));
-            }
+            init();
         }
 
     }
